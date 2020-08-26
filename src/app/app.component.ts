647b6a9e683ae71691fe931592cd5e86a1da8bf8
Paulina Guerrero <paaulinagp@gmail.com>
2020-08-25 19:45:16 -0500

    ci: instalando fontawsome, sweetalert y bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
+
+import {
+  faCoffee,
+  faCamera,
+  faStar,
+  faArrowCircleRight,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'heroesapp';
-}
+export class AppComponent {}
